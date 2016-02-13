c4a92a20ea3485793234adb5d1b7aabe91469429
benofben <ben.lackey@outlook.com>
2016-02-13 12:43:41 -0800

    playing with dir structure some more

---
+++ bin/dse.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+./install_java.sh
+
+./os/configure.sh
+
+./dse/install.sh
+./dse/configure.sh
+./dse/start.sh
---
+++ bin/opscenter.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+./install_java.sh
+
+./opscenter/install.sh
+./opscenter/start.sh
