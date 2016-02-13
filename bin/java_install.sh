f67b20a5650b49ac3c8a847804dc898a673240c8
benofben <ben.lackey@outlook.com>
2016-02-13 12:07:06 -0800

    rename scripts

--- bin/java_install.sh
+++ bin/java_install.sh
@@ -5,4 +5,4 @@ add-apt-repository -y ppa:webupd8team/java
 apt-get -y update
 echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
 echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
-apt-get -y install oracle-java7-installer
+apt-get -y install oracle-java8-installer
