c94125cc87c468b15518a374bd548dc5ef166f2b
benofben <ben.lackey@outlook.com>
2016-02-13 13:25:42 -0800

    starting to hack the dse scripts

--- bin/dse/configure.sh
+++ bin/dse/configure.sh
@@ -1,27 +1,16 @@
 #!/usr/bin/env bash
 
-echo $(basename "$0") "$@"
-data_center_name=$1
-seed_node_public_ip=$2
-opscenter_public_ip=$3
-node_public_ip=$4
-node_private_ip=$5
+cloud_type=$1 #azure, gcp or aws
+data_center_name=$2
+seed_node_public_ip=$3
+opscenter_public_ip=$4
 
-#azure, gcp or aws
-cloud_type=$6
+# probably ought to use the appropriate metadata service instead of this
+node_public_ip=`curl --max-time 50000 --retry 12 --retry-delay 50000 -s 'http://checkip.dyndns.org' | sed 's/.*Current IP Address: \([0-9\.]*\).*/\1/g'`
 
-#Make sure file systems were built
-data_file_directories="/mnt/cassandra/data"
-commitlog_directory="/mnt/cassandra/commitlog"
-saved_caches_directory="/mnt/cassandra/saved_caches"
+node_private_ip=`echo $(hostname -I)`
 
-if [ -n $data_center_name ]; then		data_center_name=`printf '%s' 'data_center_name: ' >&2; read val; echo $val;`; fi
-if [ -n $seed_node_public_ip ]; then	seed_node_public_ip=$default_seed_node_public_ip; fi
-if [ -n $opscenter_public_ip ]; then 	opscenter_public_ip=$default_opscenter_public_ip; fi
-if [ -n $node_public_ip ]; then 		node_public_ip=`curl --max-time 50000 --retry 12 --retry-delay 50000 -s 'http://checkip.dyndns.org' | sed 's/.*Current IP Address: \([0-9\.]*\).*/\1/g'`; fi
-if [ -n $node_private_ip ]; then		node_private_ip=`echo $(hostname -I)`; fi
-
-echo ''
+echo "Configuring nodes with the settings:"
 echo data_center_name \'$data_center_name\'
 echo seed_node_public_ip \'$seed_node_public_ip\'
 echo opscenter_public_ip \'$opscenter_public_ip\'
