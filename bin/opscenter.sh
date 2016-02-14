03ddad9e3d6903ffd0a03200047571651856a7b7
benofben <ben.lackey@outlook.com>
2016-02-14 13:48:16 -0800

    still pretty hard coded, but closer

--- bin/opscenter.sh
+++ bin/opscenter.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
+seed_node_public_ip="13.94.43.68"
+
 ./os/install_java.sh
 
 ./opscenter/install.sh
 ./opscenter/start.sh
+./opscenter/manage_existing_cluster.sh $seed_node_public_ip
