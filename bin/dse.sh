c08bab27a15d8652d9dd1f95838a1e4457fb2034
benofben <ben.lackey@outlook.com>
2016-02-14 12:20:19 -0800

    dropping cqlsh config for now

--- bin/dse.sh
+++ bin/dse.sh
@@ -2,18 +2,16 @@
 
 # hard coding some things for now...
 
-# want to support azure, aws and google
 cloud_type="azure"
 data_center_name="dc0"
 
 # just going to hard code a private ip for now...
-# This is the lowest usable ip in our range.
 seed_node_public_ip="10.0.0.4"
+
+# and ignore opscenter...
 opscenter_public_ip=""
 
-# probably ought to use the appropriate metadata service instead of this
 node_public_ip=`curl --max-time 50000 --retry 12 --retry-delay 50000 -s 'http://checkip.dyndns.org' | sed 's/.*Current IP Address: \([0-9\.]*\).*/\1/g'`
-
 node_private_ip=`echo $(hostname -I)`
 
 echo "Configuring nodes with the settings:"
