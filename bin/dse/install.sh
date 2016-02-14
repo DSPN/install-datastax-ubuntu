ea7d9868b140cbcbf8c23ae43aa52a3cccc2a421
benofben <ben.lackey@outlook.com>
2016-02-14 12:36:17 -0800

    put that chown in the right file...

--- bin/dse/install.sh
+++ bin/dse/install.sh
@@ -5,3 +5,7 @@ echo "deb https://datastax%40microsoft.com:3A7vadPHbNT@debian.datastax.com/enter
 curl -L https://debian.datastax.com/debian/repo_key | sudo apt-key add -
 apt-get -y update
 apt-get -y install dse-full=4.8.4-1
+
+# The install creates a cassandra user, so now we can do this:
+chown cassandra /mnt
+chgrp cassandra /mnt
