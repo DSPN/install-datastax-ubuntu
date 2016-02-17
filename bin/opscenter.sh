22210154c9641ac5a02af5ea4ca7fb87ee7d3ae0
benofben <ben.lackey@outlook.com>
2016-02-16 18:11:00 -0800

    drop config changes that didn't work out

--- bin/opscenter.sh
+++ bin/opscenter.sh
@@ -4,13 +4,5 @@ seed_node_public_ip=$1
 
 ./os/install_java.sh
 ./opscenter/install.sh
-
-cat <</EOF >> /etc/opscenter/opscenterd.conf
-
-[clusters]
-add_cluster_timeout = 90
-/EOF
-
 ./opscenter/start.sh
-
 #./opscenter/manage_existing_cluster.sh $seed_node_public_ip
