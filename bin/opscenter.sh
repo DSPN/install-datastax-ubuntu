cc4a393e8aa3c0ccd9a7d3b316b18b29e753f728
benofben <ben.lackey@outlook.com>
2016-02-17 18:13:03 -0800

    changes per OpsCenter team

--- bin/opscenter.sh
+++ bin/opscenter.sh
@@ -5,4 +5,7 @@ seed_node_public_ip=$1
 ./os/install_java.sh
 ./opscenter/install.sh
 ./opscenter/start.sh
-#./opscenter/manage_existing_cluster.sh $seed_node_public_ip
+
+echo "Waiting for OpsCenter to start..."
+sleep 30
+./opscenter/manage_existing_cluster.sh $seed_node_public_ip
