ae852381ba4b7d4205f7c934ca4801c3c6c11cf5
benofben <ben.lackey@outlook.com>
2016-02-19 12:48:59 -0800

    drop opsc from agent

--- bin/dse.sh
+++ bin/dse.sh
@@ -3,7 +3,6 @@
 cloud_type=$1
 data_center_name=$2
 seed_node_public_ip=$3
-opscenter_public_ip=$4
 
 node_public_ip=`curl --retry 10 icanhazip.com`
 node_private_ip=`echo $(hostname -I)`
@@ -12,7 +11,6 @@ echo "Configuring nodes with the settings:"
 echo cloud_type \'$cloud_type\'
 echo data_center_name \'$data_center_name\'
 echo seed_node_public_ip \'$seed_node_public_ip\'
-echo opscenter_public_ip \'$opscenter_public_ip\'
 echo node_public_ip \'$node_public_ip\'
 echo node_private_ip \'$node_private_ip\'
 
@@ -24,5 +22,5 @@ echo node_private_ip \'$node_private_ip\'
 ./dse/install.sh
 ./dse/configure_cassandra_rackdc_properties.sh $cloud_type $data_center_name
 ./dse/configure_cassandra_yaml.sh $node_private_ip $node_public_ip $seed_node_public_ip
-./dse/configure_agent_address_yaml.sh $node_private_ip $node_public_ip $opscenter_public_ip
+./dse/configure_agent_address_yaml.sh $node_private_ip $node_public_ip
 ./dse/start.sh
--- bin/dse/configure_agent_address_yaml.sh
+++ bin/dse/configure_agent_address_yaml.sh
@@ -2,7 +2,6 @@
 
 node_private_ip=$1
 node_public_ip=$2
-opscenter_public_ip=$3
 
 # looks like the agent creates an empty /var/lib/datastax-agent/conf/address.yaml when it starts for the first time
 # given that, we're not going to worry about backing it up
@@ -10,7 +9,6 @@ opscenter_public_ip=$3
 file=/var/lib/datastax-agent/conf/address.yaml
 
 cat <</EOF > $file
-stomp_interface: $opscenter_public_ip
 agent_rpc_interface: $node_private_ip
 agent_rpc_broadcast_address: $node_public_ip
 /EOF
