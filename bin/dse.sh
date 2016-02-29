a6f708a4a95e0c977d18b7bf26d3621c27b8ff10
benofben <ben.lackey@outlook.com>
2016-02-29 13:33:33 -0600

    not sure what that awk statement was supposed to do....

--- bin/dse.sh
+++ bin/dse.sh
@@ -8,7 +8,7 @@ seed_nodes_dns_names=$3
 
 # Assuming only one seed is passed in for now
 seed_node_dns_name=$seed_node_dns_names
-seed_node_public_ip=`dig +short $seed_node_dns_name | awk '{ print ; exit }'`
+seed_node_public_ip=`dig +short $seed_node_dns_name`
 
 node_public_ip=`curl --retry 10 icanhazip.com`
 node_private_ip=`echo $(hostname -I)`
