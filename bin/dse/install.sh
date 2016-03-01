c7a72d8b4498c4f9957b0f0be2d68ede4f318260
benofben <ben.lackey@outlook.com>
2016-03-01 11:45:27 -0600

    move to 4.8.5-1

--- bin/dse/install.sh
+++ bin/dse/install.sh
@@ -4,7 +4,7 @@ echo "Installing DataStax Enterprise"
 echo "deb https://datastax%40microsoft.com:3A7vadPHbNT@debian.datastax.com/enterprise stable main" | sudo tee -a /etc/apt/sources.list.d/datastax.sources.list
 curl -L https://debian.datastax.com/debian/repo_key | sudo apt-key add -
 apt-get -y update
-apt-get -y install dse-full=4.8.4-1
+apt-get -y install dse-full=4.8.5-1
 
 # The install of dse creates a cassandra user, so now we can do this:
 chown cassandra /mnt
