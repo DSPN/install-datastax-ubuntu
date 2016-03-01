ad16b0729ddbaf86d9640b61f74e86c0a7cc1f40
benofben <ben.lackey@outlook.com>
2016-03-01 15:31:58 -0600

    forgot -y

--- bin/dse/install.sh
+++ bin/dse/install.sh
@@ -6,7 +6,7 @@ curl -L https://debian.datastax.com/debian/repo_key | sudo apt-key add -
 apt-get -y update
 
 dse_version=4.8.5-1
-apt-get install dse-full=$dse_version dse=$dse_version dse-hive=$dse_version dse-pig=$dse_version dse-demos=$dse_version dse-libsolr=$dse_version dse-libtomcat=$dse_version dse-libsqoop=$dse_version dse-liblog4j=$dse_version dse-libmahout=$dse_version dse-libhadoop-native=$dse_version dse-libcassandra=$dse_version dse-libhive=$dse_version dse-libpig=$dse_version dse-libhadoop=$dse_version dse-libspark=$dse_version
+apt-get -y install dse-full=$dse_version dse=$dse_version dse-hive=$dse_version dse-pig=$dse_version dse-demos=$dse_version dse-libsolr=$dse_version dse-libtomcat=$dse_version dse-libsqoop=$dse_version dse-liblog4j=$dse_version dse-libmahout=$dse_version dse-libhadoop-native=$dse_version dse-libcassandra=$dse_version dse-libhive=$dse_version dse-libpig=$dse_version dse-libhadoop=$dse_version dse-libspark=$dse_version
 
 # The install of dse creates a cassandra user, so now we can do this:
 chown cassandra /mnt
