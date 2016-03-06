5bb1c8b76f470c52818b55072ca33e9c7339136d
benofben <ben.lackey@outlook.com>
2016-03-06 15:50:19 -0800

    typo

--- bin/opscenter.sh
+++ bin/opscenter.sh
@@ -5,7 +5,7 @@ echo "Running install-datastax/bin/opscenter.sh"
 seed_nodes_dns_names=$1
 
 # Assuming only one seed is passed in for now
-seed_node_dns_name=$seed_node_dns_names
+seed_node_dns_name=$seed_nodes_dns_names
 
 # On AWS and Azure this gets the public IP.  On Google it resolves to a private IP that is globally routeable in GCP.
 seed_node_ip=`dig +short $seed_node_dns_name`
