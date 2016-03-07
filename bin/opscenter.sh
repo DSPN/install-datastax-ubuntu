7f567ddc8efcc0100dce8b823514214d3e04a42d
benofben <ben.lackey@outlook.com>
2016-03-06 21:57:35 -0600

    drop useless wait

--- bin/opscenter.sh
+++ bin/opscenter.sh
@@ -18,5 +18,5 @@ echo seed_node_ip \'$seed_node_ip\'
 ./opscenter/start.sh
 
 echo "Waiting for OpsCenter and nodes to start..."
-sleep 500
+sleep 60
 ./opscenter/manage_existing_cluster.sh $seed_node_ip
