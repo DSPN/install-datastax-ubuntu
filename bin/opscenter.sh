89a1de45b45119ed2950dc84ba37e85e7a59726a
benofben <ben.lackey@outlook.com>
2016-03-22 14:52:34 -0700

    add opscenterd config

--- bin/opscenter.sh
+++ bin/opscenter.sh
@@ -20,6 +20,12 @@ fi
 
 ./os/install_java.sh
 ./opscenter/install.sh
+
+if [[ $cloud_type == "azure" ]]; then
+  opscenter_broadcast_ip=`curl --retry 10 icanhazip.com`
+  ./opscenter/configure_opscenterd_conf.sh $opscenter_broadcast_ip
+fi
+
 ./opscenter/start.sh
 
 echo "Waiting for OpsCenter to start..."
---
+++ bin/opscenter/configure_opscenterd_conf.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+opscenter_broadcast_ip=$1
+
+file="/etc/opscenter/opscenterd.conf"
+
+echo "Adding the broadcast IP to opscenterd.conf"
+echo "[agents]" >> $file
+echo "reported_interface=$opscenter_broadcast_ip" >> $file
+echo "" >> $file
