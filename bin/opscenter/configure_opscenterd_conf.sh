5f0881a6410a826def4fae8b8b36eea8a9646eeb
benofben <ben.lackey@outlook.com>
2016-03-23 13:42:21 -0700

    add debug

--- bin/opscenter/configure_opscenterd_conf.sh
+++ bin/opscenter/configure_opscenterd_conf.sh
@@ -5,6 +5,11 @@ opscenter_broadcast_ip=$1
 file="/etc/opscenter/opscenterd.conf"
 
 echo "Adding the broadcast IP to opscenterd.conf"
+
 echo "[agents]" >> $file
 echo "reported_interface=$opscenter_broadcast_ip" >> $file
+
+echo "[logging]" >> $file
+echo "level=debug" >> $file
+
 echo "" >> $file
