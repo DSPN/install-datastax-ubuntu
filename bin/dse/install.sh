bc2b285b99dac498e713b5203ba6cecdcccb8587
benofben <ben.lackey@outlook.com>
2016-05-11 14:19:26 -0700

    reorder install scripts

--- bin/dse/install.sh
+++ bin/dse/install.sh
@@ -4,6 +4,7 @@ cloud_type=$1
 
 echo "Installing DataStax Enterprise"
 
+echo "Adding the DataStax repository"
 if [[ $cloud_type == "azure" ]]; then
   echo "deb https://datastax%40microsoft.com:3A7vadPHbNT@debian.datastax.com/enterprise stable main" | sudo tee -a /etc/apt/sources.list.d/datastax.sources.list
 elif [[ $cloud_type == "gce" ]] || [[ $cloud_type == "gke" ]]; then
@@ -13,13 +14,15 @@ else
   exit 99
 fi
 
-curl -L https://debian.datastax.com/debian/repo_key | sudo apt-key add -
-
-# Having https issues on GKE.  Going to try this.
+echo "Installing HTTPS"
 apt-get -y update
 apt-get -y install apt-transport-https
 
+curl -L https://debian.datastax.com/debian/repo_key | sudo apt-key add -
+
+echo "Running apt-get install dse..."
 dse_version=4.8.5-1
+apt-get -y update
 apt-get -y install dse-full=$dse_version dse=$dse_version dse-hive=$dse_version dse-pig=$dse_version dse-demos=$dse_version dse-libsolr=$dse_version dse-libtomcat=$dse_version dse-libsqoop=$dse_version dse-liblog4j=$dse_version dse-libmahout=$dse_version dse-libhadoop-native=$dse_version dse-libcassandra=$dse_version dse-libhive=$dse_version dse-libpig=$dse_version dse-libhadoop=$dse_version dse-libspark=$dse_version
 
 # The install of dse creates a cassandra user, so now we can do this:
