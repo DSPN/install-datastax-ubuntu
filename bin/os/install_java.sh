a91ada6fc61728887e43aa484c2bf8717045fdb0
benofben <ben.lackey@outlook.com>
2016-06-09 11:14:03 -0700

    retry java install

--- bin/os/install_java.sh
+++ bin/os/install_java.sh
@@ -10,3 +10,8 @@ apt-get -y update
 echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
 echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
 apt-get -y install oracle-java8-installer
+
+# We're seeing Java installs fail intermittently.  Retrying indefinitely seems problematic.  I'm not sure
+# what the correct solution is.  For now, we're just going to run the install a second time.  This will do
+# nothing if the first install was successful and I suspect will eliminate the majority of our failures.
+apt-get -y install oracle-java8-installer
