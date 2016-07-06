aee6c3eb3be8cf888d8475a5a3f3c69899363f12
Ankur Thuse <ankur.thuse@gmail.com>
2016-07-06 11:16:47 -0700

    Changed OpsC install.sh

--- bin/opscenter/install.sh
+++ bin/opscenter/install.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
 echo "Installing OpsCenter"
-echo "deb http://debian.datastax.com/community stable main" | sudo tee -a /etc/apt/sources.list.d/datastax.community.list
+#echo "deb http://debian.datastax.com/community stable main" | sudo tee -a /etc/apt/sources.list.d/datastax.community.list
+echo "Adding the DataStax repository"
+if [[ $cloud_type == "gce" ]] || [[ $cloud_type == "gke" ]]; then
+  echo "deb http://datastax%40google.com:8GdeeVT2s7zi@debian.datastax.com/enterprise stable main" | sudo tee -a /etc/apt/sources.list.d/datastax.sources.list
+else
+  echo "deb http://datastax%40microsoft.com:3A7vadPHbNT@debian.datastax.com/enterprise stable main" | sudo tee -a /etc/apt/sources.list.d/datastax.sources.list
+fi
+
 curl -L http://debian.datastax.com/debian/repo_key | sudo apt-key add -
+
 apt-get update
 apt-get -y install opscenter=6.0.0
