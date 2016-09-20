47e7cab0eda99e279bad039d4433607096528de2
Gilbert Lau <gmflau@gmail.com>
2016-09-20 15:11:38 -0700

    Fixed $dc issue
    The updated CURL command can now map $dc to a C* datacenter dynamically.

--- bin/opscenter/configure_opscenter_keyspace.sh
+++ bin/opscenter/configure_opscenter_keyspace.sh
@@ -4,8 +4,9 @@ json=`curl http://127.0.0.1:8888/Test_Cluster/nodes/all/dc`
 dcs=`echo $json | grep -Po '(?<=: ")[^"]*'`
 dc=`echo $dcs | cut -d' ' -f1`
 
-curl -X PUT http://127.0.0.1:8888/Test_Cluster/keyspaces/OpsCenter -d '{
-    "strategy_class": "org.apache.cassandra.locator.NetworkTopologyStrategy",
-    "strategy_options": {"$dc": "2"},
-    "durable_writes": true
-  }'
+curl -X PUT http://127.0.0.1:8888/Test_Cluster/keyspaces/OpsCenter -d "{
+    \"strategy_class\": \"org.apache.cassandra.locator.NetworkTopologyStrategy\",
+    \"strategy_options\": {\"$dc\": \"2\"},
+    \"durable_writes\": true
+  }"
+
