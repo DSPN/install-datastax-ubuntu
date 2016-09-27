a36ca81b82d1d0a86c02473855830e8114cebdef
Collin Poczatek <collin.poczatek@gmail.com>
2016-09-26 20:20:38 -0700

    messing with opcenter ip/name

--- bin/dse.sh
+++ bin/dse.sh
@@ -42,7 +42,9 @@ elif [[ $cloud_type == "azure" ]]; then
   done
 elif [[ $cloud_type == "aws" ]]; then
   seed_node_ip=`getent hosts $seed_node_dns_name | awk '{ print $1 }'`
-  opscenter_ip=`getent hosts $opscenter_dns_name | awk '{ print $1 }'`
+  #opscenter_ip=`getent hosts $opscenter_dns_name | awk '{ print $1 }'`
+  # Playing tricks and passing EIP instead of hostname
+  opscenter_ip=opscenter_dns_name
 fi
 
 if [[ $cloud_type == "gce" ]] || [[ $cloud_type == "gke" ]]; then
