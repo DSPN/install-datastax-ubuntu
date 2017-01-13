97b235bfb403d33c58d1dda12bd03b0ea0f4a1b6
Gilbert Lau <gmflau@users.noreply.github.com>
2017-01-12 17:37:43 -0800

    Added DCOS support

--- bin/opscenter.sh
+++ bin/opscenter.sh
@@ -12,7 +12,7 @@ seed_node_dns_name=$seed_nodes_dns_names
 # On GKE we resolve to a private IP.
 # On AWS and Azure this gets the public IP.
 # On GCE it resolves to a private IP that is globally routeable in GCE.
-if [[ $cloud_type == "gke" ]]; then
+if [[ $cloud_type == "gke" ]] || [[ $cloud_type == "DCOS" ]]; then
   seed_node_ip=`getent hosts $seed_node_dns_name | awk '{ print $1w }'`
 elif [[ $cloud_type == "gce" ]]; then
   # If the IP isn't up yet it will resolve to "" on GCE
