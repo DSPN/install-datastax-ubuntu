f612cabfcc6960f351ca4b80be3bccd1d4432927
Gilbert Lau <gmflau@users.noreply.github.com>
2017-01-12 17:40:56 -0800

    Added "apt-get curl"

--- bin/dse.sh
+++ bin/dse.sh
@@ -9,6 +9,10 @@ opscenter_dns_name=$4
 dcos_container_path=$5
 TOOLS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# Get curl package
+apt-get update
+apt-get -y install curl
+
 # Assuming only one seed is passed in for now
 seed_node_dns_name=$seed_nodes_dns_names
 
