24c32f02f3638dd3ccaf7435dbc94fabd740348d
Gilbert Lau <gmflau@users.noreply.github.com>
2017-01-19 09:59:24 -0800

    It is not required for docker

--- bin/opscenter/configure_opscenterd_conf.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-opscenter_broadcast_ip=$1
-
-file="/etc/opscenter/opscenterd.conf"
-echo "Adding the broadcast IP to opscenterd.conf"
-echo "[agents]" >> $file
-echo "reported_interface=$opscenter_broadcast_ip" >> $file
-echo "" >> $file
