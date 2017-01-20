0f4919639e1469cd1418a371a923ad2ae864c60a
Gilbert Lau <gmflau@users.noreply.github.com>
2017-01-19 20:31:14 -0800

    Update configure_dse.sh

--- bin/dse/configure_dse.sh
+++ bin/dse/configure_dse.sh
@@ -20,34 +20,28 @@ backup="$file.$date"
 cp $file $backup
 
 if [ $search_enabled -eq "true" ]; then
-  cat $file \
-  | sed -e "s:.*\(SOLR_ENABLED\=\).*:SOLR_ENABLED\=1:" \
-  > $file.new
+  search_flag=1
 else
-  cat $file \
-  | sed -e "s:.*\(SOLR_ENABLED\=\).*:SOLR_ENABLED\=0:" \
-  > $file.new
+  search_flag=0
 fi
 
 if [ $analytics_enabled -eq "true" ]; then
-  cat $file.new \
-  | sed -e "s:.*\(SPARK_ENABLED\=\).*:SPARK_ENABLED\=1:" \
-  > $file.new
+  analytics_flag=1
 else
-  cat $file.new \
-  | sed -e "s:.*\(SPARK_ENABLED\=\).*:SPARK_ENABLED\=0:" \
-  > $file.new
+  analytics_flag=0
 fi
 
 if [ $graph_enabled -eq "true" ]; then
-  cat $file.new \
-  | sed -e "s:.*\(GRAPH_ENABLED\=\).*:GRAPH_ENABLED\=1:" \
-  > $file.new
+  graph_flag=1
 else
-  cat $file.new \
-  | sed -e "s:.*\(GRAPH_ENABLED\=\).*:GRAPH_ENABLED\=0:" \
-  > $file.new
+  graph_flag=0
 fi
+
+cat $file \
+| sed -e "s:.*\(SOLR_ENABLED\=\).*:SOLR_ENABLED\=$search_flag:" \
+| sed -e "s:.*\(SPARK_ENABLED\=\).*:SPARK_ENABLED\=$analytics_flag:" \
+| sed -e "s:.*\(GRAPH_ENABLED\=\).*:GRAPH_ENABLED\=$graph_flag:" \
+> $file.new
 
 mv $file.new $file
 
