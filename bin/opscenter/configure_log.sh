f70e63f03b8be9b78322ff3ca581527384046f0b
Gilbert Lau <gmflau@users.noreply.github.com>
2017-01-22 01:48:39 -0800

    Update configure_log.sh

--- bin/opscenter/configure_log.sh
+++ bin/opscenter/configure_log.sh
@@ -18,6 +18,8 @@ cat $file \
 | sed -e "s:<fileNamePattern>/var/log/opscenter/opscenterd.%i.log</fileNamePattern>$:<fileNamePattern>$dcos_opscenter_pv/log/opscenter/opscenterd.%i.log</fileNamePattern>:" \
 | sed -e "s:<file>/var/log/opscenter/repair_service/${cluster_id}.log</file>$:<file>/mnt/log/opscenter/opscenterd.log</file>:" \
 | sed -e "s:<fileNamePattern>/var/log/opscenter/repair_service/${cluster_id}.%i.log</fileNamePattern>$:<fileNamePattern>/mnt/log/opscenter/repair_service/${cluster_id}.%i.log</fileNamePattern>:" \
+| sed -e "s:<file>/var/log/opscenter/http.log</file>$:<file>$dcos_opscenter_pv/log/opscenter/http.log</file>:" \
+| sed -e "s:<fileNamePattern>/var/log/opscenter/http.%i.log</fileNamePattern>$:<fileNamePattern>$dcos_opscenter_pv/log/opscenter/http.%i.log</fileNamePattern>:" \
 > $file.new
 
 mv $file.new $file
