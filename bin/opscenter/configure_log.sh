f35b2f51473a0963ee5355b0b169c78e9e9c89a1
Gilbert Lau <gmflau@users.noreply.github.com>
2017-01-22 01:39:40 -0800

    Update configure_log.sh

--- bin/opscenter/configure_log.sh
+++ bin/opscenter/configure_log.sh
@@ -6,7 +6,7 @@ echo dcos_opscenter_pv \'$dcos_opscenter_pv\'
 file=/etc/opscenter/logback.xml
 
 # Create a log folder (log/opscenter) in persistent volume
-mkdir -p $dcos_opscenter_pv/log/opscenter
+mkdir -p $dcos_opscenter_pv/log/opscenter/repair_service
 chown -R opscenter:opscenter $dcos_opscenter_pv/log/opscenter
 
 date=$(date +%F)
@@ -16,6 +16,8 @@ cp $file $backup
 cat $file \
 | sed -e "s:<file>/var/log/opscenter/opscenterd.log</file>$:<file>$dcos_opscenter_pv/log/opscenter/opscenterd.log</file>:" \
 | sed -e "s:<fileNamePattern>/var/log/opscenter/opscenterd.%i.log</fileNamePattern>$:<fileNamePattern>$dcos_opscenter_pv/log/opscenter/opscenterd.%i.log</fileNamePattern>:" \
+| sed -e "s:<file>/var/log/opscenter/repair_service/${cluster_id}.log</file>$:<file>/mnt/log/opscenter/opscenterd.log</file>:" \
+| sed -e "s:<fileNamePattern>/var/log/opscenter/repair_service/${cluster_id}.%i.log</fileNamePattern>$:<fileNamePattern>/mnt/log/opscenter/repair_service/${cluster_id}.%i.log</fileNamePattern>:" \
 > $file.new
 
 mv $file.new $file
