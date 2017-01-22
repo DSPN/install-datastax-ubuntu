4b514a0f22b370a4d460e8c4b837941c4a16d1f7
Gilbert Lau <gmflau@users.noreply.github.com>
2017-01-21 21:34:21 -0800

    Update configure_log.sh

--- bin/opscenter/configure_log.sh
+++ bin/opscenter/configure_log.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 
-dse_opscenter_pv=$1
-echo dse_opscenter_pv \'$dse_opscenter_pv\'
+dcos_opscenter_pv=$1
+echo dcos_opscenter_pv \'$dcos_opscenter_pv\'
 
 file=/etc/opscenter/logback.xml
 
 # Create a log folder (log/opscenter) in persistent volume
-mkdir -p $dse_opscenter_pv/log/opscenter
-chown -r opscenter $dse_opscenter_pv/log/opscenter
-chgrp -r opscenter $dse_opscenter_pv/log/opscenter
+mkdir -p $dcos_opscenter_pv/log/opscenter
+chown -r opscenter $dcos_opscenter_pv/log/opscenter
+chgrp -r opscenter $dcos_opscenter_pv/log/opscenter
 
 date=$(date +%F)
 backup="$file.$date"
 cp $file $backup
 
 cat $file \
-| sed -e "s:<file>/var/log/opscenter/opscenterd.log</file>$:<file>$dse_opscenter_pv/log/opscenter/opscenterd.log</file>:" \
+| sed -e "s:<file>/var/log/opscenter/opscenterd.log</file>$:<file>$dcos_opscenter_pv/log/opscenter/opscenterd.log</file>:" \
 > $file.new
 
 mv $file.new $file
