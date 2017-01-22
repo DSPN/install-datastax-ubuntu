d75510f5debb6bcaaacf52d14b858e55dbc2ad01
Gilbert Lau <gmflau@users.noreply.github.com>
2017-01-21 16:10:19 -0800

    Update configure_log.sh

--- bin/dse/configure_log.sh
+++ bin/dse/configure_log.sh
@@ -4,6 +4,8 @@ dse_log_path=$1/log
 echo dse_log_path \'$dse_log_path\'
 
 mkdir $dse_log_path
+chown cassandra $dse_log_path
+chgrp cassandra $dse_log_path
 
 file=/etc/dse/cassandra/cassandra-env.sh
 
