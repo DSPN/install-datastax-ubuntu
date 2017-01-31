0d27aba82bb1d999dd0f71a5cada95eeae343d81
Gilbert Lau <gmflau@users.noreply.github.com>
2017-01-31 09:39:29 -0800

    Update configure_cassandra_rackdc_properties.sh

--- bin/dse/configure_cassandra_rackdc_properties.sh
+++ bin/dse/configure_cassandra_rackdc_properties.sh
@@ -1,43 +1,5 @@
 #!/usr/bin/env bash
 
-function get_rack {
-  cloud_type=$1
-
-  echo CLOUD_type inside get_rack = $cloud_type 1>&2
-
-  if [[ $cloud_type == "azure" ]]; then
-    fault_domain=$(curl --max-time 50000 --retry 12 --retry-delay 50000 http://169.254.169.254/metadata/v1/InstanceInfo -s -S | sed -e 's/.*"FD":"\([^"]*\)".*/\1/')
-    if [ ! "$fault_domain" ]; then
-      echo Unable to retrieve Instance Fault Domain from instance metadata server 1>&2
-	  exit 99
-    fi
-    rack="FD$fault_domain"
-  elif [[ $cloud_type == "aws" ]]; then
-    availability_zone=$( curl --max-time 50000 --retry 12 --retry-delay 50000 http://169.254.169.254/latest/meta-data/placement/availability-zone -s -S )
-    if [ ! "$availability_zone" ]; then
-	  echo Unable to retrieve Instance Availability Zone from instance metadata server 1>&2
-	  exit 99
-    fi
-    rack=$(echo $availability_zone | sed -e 's/-/_/g')
-  elif [[ $cloud_type == "gce" ]]; then
-    zone=$(curl -s -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/zone" | grep -o [[:alnum:]-]*$)
-    if [ ! "$zone" ]; then
-	  echo Unable to retrieve Instance Zone from instance metadata server 1>&2
-	  exit 99
-    fi
-    rack=$zone
-  elif [[ $cloud_type == "gke" ]]; then
-    rack="rack1"
-  elif [[ $cloud_type == "DCOS" ]]; then
-    rack="rack1"
-  else
-    echo Cloud type $cloud_type is not supported 1>&2
-    exit 99
-  fi
-
-  echo $rack
-}
-
 cloud_type="$1"
 dc="$2"
 echo CLOUD_type = $cloud_type 1>&2
