01fce0f31bc3e3c9fba3d88891cfe38cf15112cf
Gilbert Lau <gmflau@users.noreply.github.com>
2017-07-04 22:24:53 -0700

    Bumped it up to DSE 5.1.2-1

--- bin/dse/install.sh
+++ bin/dse/install.sh
@@ -6,7 +6,7 @@ cloud_type=$1
 # Overidable by setting env var in calling template,
 # eg: export OPSC_VERSION='6.1.0'
 
-dse_version=5.0.6-1
+dse_version=5.1.2-1
 opscenter_version=6.1.0
 
 if [ -z "$OPSC_VERSION" ]
@@ -38,8 +38,7 @@ curl -L http://debian.datastax.com/debian/repo_key | sudo apt-key add -
 apt-get -y update
 
 echo "Running apt-get install dse"
-
-apt-get -y install dse-full=$dse_version dse=$dse_version dse-hive=$dse_version dse-pig=$dse_version dse-demos=$dse_version dse-libsolr=$dse_version dse-libtomcat=$dse_version dse-libsqoop=$dse_version dse-liblog4j=$dse_version dse-libmahout=$dse_version dse-libhadoop-native=$dse_version dse-libcassandra=$dse_version dse-libhive=$dse_version dse-libpig=$dse_version dse-libhadoop=$dse_version dse-libspark=$dse_version dse-libhadoop2-client-native=$dse_version dse-libgraph=$dse_version dse-libhadoop2-client=$dse_version
+apt-get -y install dse-full=$dse_version dse=$dse_version dse-demos=$dse_version dse-libsolr=$dse_version dse-libtomcat=$dse_version dse-liblog4j=$dse_version dse-libcassandra=$dse_version dse-libspark=$dse_version dse-libhadoop2-client-native=$dse_version dse-libgraph=$dse_version dse-libhadoop2-client=$dse_version
 
 echo "Running apt-get install datastax-agent"
 apt-get -y install datastax-agent=$opscenter_version
