fb01831c9c06b228dd1fc15d656237c7caf07fd9
Collin Poczatek <collin.poczatek@gmail.com>
2017-07-18 09:53:57 -0700

    add dc level install option

--- bin/lcm/triggerInstall.py
+++ bin/lcm/triggerInstall.py
@@ -7,12 +7,13 @@ import os
 import utilLCM as lcm
 
 def setupArgs():
-    parser = argparse.ArgumentParser(description='Setup LCM managed DSE cluster, repo, config, and ssh creds')
+    parser = argparse.ArgumentParser(description='Trigger LCM install job after last node posts.')
     required = parser.add_argument_group('Required named arguments')
     required.add_argument('--opsc-ip', required=True, type=str, help='public ip of OpsCenter instance')
     required.add_argument('--clustername', required=True, type=str, help='Name of cluster.')
     required.add_argument('--clustersize', type=int, default=0, help='Trigger install job when clustersize nodes have posted')
     parser.add_argument('--dbpasswd', type=str, default='cassandra', help='DB password.')
+    parser.add_argument('--dclevel', action='store_true', help='Trigger DC level install job(s).' )
     return parser
 
 def main():
@@ -27,7 +28,15 @@ def main():
         if r['name'] == args.clustername:
             cid = r['id']
     lcm.waitForNodes(numnodes=args.clustersize, pause=6, trys=400)
-    lcm.triggerInstall(cid, None, args.dbpasswd)
+    if args.dclevel:
+        datacenters = requests.get("http://{url}/api/v1/lcm/datacenters/".format(url=lcm.opsc_url)).json()
+        for r in datacenters['results']:
+            dcid = r['id']
+            print("Triggering install for DC, id = {i}".format(i=dcid))
+            lcm.triggerInstall(None, dcid, args.dbpasswd)
+    else:
+        print("Triggering install for cluster, id = {i}".format(i=cid))
+        lcm.triggerInstall(cid, None, args.dbpasswd)
 
 # ----------------------------
 if __name__ == "__main__":
