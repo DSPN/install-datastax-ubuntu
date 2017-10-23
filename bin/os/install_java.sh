ad85c045320c4c0dccd2f8560ec52614141816ac
Collin Poczatek <collin.poczatek@gmail.com>
2017-10-23 10:42:13 -0700

    switch to java tar install

--- bin/os/install_java.sh
+++ bin/os/install_java.sh
@@ -2,16 +2,18 @@
 
 echo "Installing the Oracle JDK"
 
-# Install add-apt-repository
+# Install add-apt-repository, removeable?
 apt-get -y install software-properties-common
 
-add-apt-repository -y ppa:webupd8team/java
-apt-get -y update
-echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
-echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
-apt-get -y install oracle-java8-installer
+wget --no-cookies --no-check-certificate \
+--header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" \
+"http://download.oracle.com/otn-pub/java/jdk/8u151-b12/e758a0de34e24606bca991d704f6dcbf/jdk-8u151-linux-x64.tar.gz"
 
-# We're seeing Java installs fail intermittently.  Retrying indefinitely seems problematic.  I'm not sure
-# what the correct solution is.  For now, we're just going to run the install a second time.  This will do
-# nothing if the first install was successful and I suspect will eliminate the majority of our failures.
-apt-get -y install oracle-java8-installer
+tar -xvf jdk-8u151-linux-x64.tar.gz -C /opt/
+
+update-alternatives --install /usr/bin/java java /opt/jdk1.8.0_151/bin/java 100
+update-alternatives --set java /opt/jdk1.8.0_151/bin/java
+update-alternatives --install /usr/bin/javac javac /opt/jdk1.8.0_151/bin/javac 100
+update-alternatives --set javac /opt/jdk1.8.0_151/bin/javac
+update-alternatives --install /usr/bin/keytool keytool /opt/jdk1.8.0_151/bin/keytool 100
+update-alternatives --set keytool /opt/jdk1.8.0_151/bin/keytool
