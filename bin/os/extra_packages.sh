696a22b9b6430a7b21b248556ca03b702c4926bb
Collin Poczatek <collin.poczatek@gmail.com>
2018-07-11 10:25:17 -0700

    add zip

--- bin/os/extra_packages.sh
+++ bin/os/extra_packages.sh
@@ -6,7 +6,7 @@ while ps -A | grep -e apt -e dpkg >/dev/null 2>&1; do sleep 10s; done;
 echo -e "No other procs: $(date +%r)"
 
 apt-get update
-apt-get -y install unzip python-pip jq sysstat
+apt-get -y install zip unzip python-pip jq sysstat
 
 # install requests pip pacakge
 pip install requests
