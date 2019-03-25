16c44ccfca8c2723017138bcd5014bffe4d2e829
Scott Hendrickson <scott.hendrickson@datastax.com>
2019-03-25 18:07:23 -0400

    disable apt-daily

--- bin/opscenter/install.sh
+++ bin/opscenter/install.sh
@@ -38,9 +38,17 @@ echo -e "Checking if apt/dpkg running, start: $(date +%r)"
 #    echo "" &> /dev/null
 #  fi
 
-  sleep 1200
+#  sleep 1200
 
 #done
+systemctl stop apt-daily.service
+systemctl kill --kill-who=all apt-daily.service
+
+# wait until `apt-get updated` has been killed
+while ! (systemctl list-units --all apt-daily.service | fgrep -q dead)
+do
+  sleep 1;
+done
 echo -e "No other procs: $(date +%r)"
 curl -L http://debian.datastax.com/debian/repo_key | sudo apt-key add -
 
