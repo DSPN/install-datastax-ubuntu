dfb5df71d179ff828b8f8c69c9a6a0403262abd3
Scott Hendrickson <scott.hendrickson@datastax.com>
2019-04-03 08:34:10 -0400

    batch-size set to 6

--- bin/lcm/utilLCM.py
+++ bin/lcm/utilLCM.py
@@ -286,7 +286,7 @@ class OpsCenter:
                "resource-id":r_id,
                "auto-bootstrap":False,
                "concurrency-strategy":"cluster-at-a-time",
-               "batch-size": 2,
+               "batch-size": 6,
                "continue-on-error":True}
         data = json.dumps(job)
         response = self.session.post("{url}/api/v2/lcm/actions/install".format(url=self.url), data=data).json()
